6c9e33c73d78ed104c49a42589cbc2f3a9c1755c
mnmly <i.am@mnmly.com>
2019-06-12 22:56:57 +0100

    Initial commit

---
+++ Comp-Cloner.jsx
@@ -0,0 +1,64 @@
+#include "json2.jsx"
+
+var data = loadJSON()
+var prototypeComp = null
+
+for (var i = 1; i <= app.project.numItems; i++) {
+    var item = app.project.item(i)
+    if (item instanceof CompItem && item.name == 'Prototype') {
+        prototypeComp = item
+        break;
+    }
+}
+
+alert(data)
+alert(prototypeComp)
+
+if (prototypeComp && data) {
+    for (var i = 0; i < data.length; i++) {
+        var dataObj = data[i]
+        var newComp = duplicateComp(prototypeComp, 'Title_' + data[i]['ID'] + '_' + data[i]['Student Name'])
+        applyDataToComp(dataObj, newComp)
+    }
+}
+
+function loadJSON(){
+    var myJSONobject;
+    var jsonFilepath = File.openDialog("Selection prompt");
+    var jsonFile = File(jsonFilepath);
+    var content;
+    if(jsonFile != false){
+        jsonFile.open('r');
+        content = jsonFile.read();
+alert(content)
+        try {
+            var json = JSON.parse(content);
+        } catch(e) {
+            alert(e)
+        }
+        jsonFile.close(); // always close files after reading
+        return json
+    }else{
+        return null
+    }
+};
+
+function duplicateComp( comp , newName) {
+    // add a page at the end of the current document,
+    // place a text frame on it, and link the text frame
+    var newComp = comp.duplicate()
+    newComp.name = newName
+    return newComp
+}
+
+function applyDataToComp( data, comp ) {
+
+    for ( var i = 1; i <= comp.numLayers; i++ ) {
+        var layer = comp.layer( i )
+        var value = data[ layer.name ]
+        if ( value && layer instanceof TextLayer ) {
+            layer.property("Source Text").setValue(value);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# AfterEffects Clone Comp
+
+Duplicate `Prototype` comp, and fill out the text layer based on the JSON data entries.
+
+![Screenshot](screenshot.png)
+
+`json
+[
+	{
+		"Course": "Course Name A",
+		"Student Name": "student name 1",
+		"Project Title": "Project Title X"
+	},
+	{
+		"Course": "Course Name B",
+		"Student Name": "student name 2",
+		"Project Title": "Project Title Y"
+	}
+]
+`
+
+### Usage
+
+- Select `Comp-Cloner.jsx` from `File` → `Script` → `Run Script File…`
